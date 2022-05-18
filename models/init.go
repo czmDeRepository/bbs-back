b16b89ced8d0333893cbf75adf7713dba0d3b425
CZM <2723081630@qq.com>
2022-05-18 22:55:16 +0800

    feat: add log Information task

--- models/init.go
+++ models/init.go
@@ -9,6 +9,7 @@ import (
 	"github.com/beego/beego/v2/client/orm"
 	"github.com/beego/beego/v2/core/logs"
 	beego "github.com/beego/beego/v2/server/web"
+	"github.com/robfig/cron"
 )
 
 func Init() {
@@ -17,6 +18,19 @@ func Init() {
 	}
 	orm.RegisterModel(new(dao.Article), new(dao.Category), new(dao.Comment), new(dao.Label), new(dao.User), new(dao.Message))
 	dao.ORM = orm.NewOrm()
+	// 总论贴量和阅读量统计
+	task := cron.New()
+	// 两小时一次
+	task.AddFunc("0 0 0/5 * * ?", RefreshInformation)
+	RefreshInformation()
+	task.Start()
+
+	// 监控大盘信息
+	monitor.Init()
+}
+
+// RefreshInformation 系统信息
+func RefreshInformation() {
 	article := new(dao.Article)
 	totalArticleNum, _ := article.Count(nil)
 	if err := storage.GetRedisPool().Set(information.TOTAL_ARTICLE_NUM, totalArticleNum); err != nil {
@@ -26,6 +40,4 @@ func Init() {
 	if err := storage.GetRedisPool().Set(information.TOTAL_READ_NUM, totalReadNum); err != nil {
 		logs.Error("set totalReadNum fail, error: %s", err.Error())
 	}
-	// 监控大盘信息
-	monitor.Init()
 }
